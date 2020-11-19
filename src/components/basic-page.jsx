7feb8c8d7684a4994cd4acfdc889abe26bc03d82
Zhibin <zhibindai26@gmail.com>
2020-11-18 20:57:56 -0500

    add href for warnings

--- src/components/basic-page.jsx
+++ src/components/basic-page.jsx
@@ -22,16 +22,24 @@ function Navbar() {
       aria-label="main navigation"
     >
       <Link to="/">
-        <a className="navbar-item">Home</a>
+        <a className="navbar-item" href="/#">
+          Home
+        </a>
       </Link>
       <Link to="/find">
-        <a className="navbar-item">Find Recipes</a>
+        <a className="navbar-item" href="/#">
+          Find Recipes
+        </a>
       </Link>
       <Link to="/add">
-        <a className="navbar-item">Add Recipe</a>
+        <a className="navbar-item" href="/#">
+          Add Recipe
+        </a>
       </Link>
       <Link to="/all_recipes">
-        <a className="navbar-item">All Recipes</a>
+        <a className="navbar-item" href="/#">
+          All Recipes
+        </a>
       </Link>
     </nav>
   );
